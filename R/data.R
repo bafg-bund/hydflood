c72be8f4a35b63d652b3f021d03b51b185b32913
Arnd Weber <arnd.weber@bafg.de>
2018-07-23 09:08:07 +0200

    remove bug caused by Rdpack

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' @name spdf.active_floodplain_elbe
-#' @rdname spdf_active_floodplain_elbe
+#' @rdname spdf.active_floodplain_elbe
 #' 
 #' @title Active floodplain along River Elbe
 #' 
@@ -7,39 +7,45 @@
 #'    the German interior parts of River Elbe from the Czech border to the
 #'    weir in Geesthacht in the coordinate reference system ETRS 1989 UTM 33N.
 #' 
-#'    Originally, this polygon was produced for the floodplain status report 
-#'    (Auenzustandsbericht; \insertCite{brunotte_flussauen_2009;textual}{hydflood3}; 
-#'    \insertCite{brunotte_flussauen_data_2009;textual}{hydflood3}; 
-#'    \insertCite{bfn_auenzustandsbericht_2009;textual}{hydflood3}) 
-#'    at a scale of 1:25000. For hydflood3 it was updated with recent flood 
-#'    protection measures and manually improved with recent digital 
-#'    elevation models and aerial images at a scale < 1:10000.
+#'   Originally, this polygon was produced for the floodplain status report 
+#'   (Auenzustandsbericht; Brunotte et al. (2009), BfN (2009)) 
+#'   at a scale of 1:25000. For hydflood3 it was updated with recent flood 
+#'   protection measures and manually improved with recent digital 
+#'   elevation models and aerial images at a scale < 1:10000.
 #' 
 #' @format A \code{SpatialPolygonsDataFrame} containing 1 polygon 
-#'
-#' @references \insertAllCited{}
-#'
+#' 
+#' @references 
+#'   \insertRef{brunotte_flussauen_2009}{hydflood3}
+#'   
+#'   \insertRef{brunotte_flussauen_data_2009}{hydflood3}
+#'   
+#'   \insertRef{bfn_auenzustandsbericht_2009}{hydflood3}
+#' 
 "spdf.active_floodplain_elbe"
 
 #' @name spdf.active_floodplain_rhein
-#' @rdname spdf_active_floodplain_rhein
+#' @rdname spdf.active_floodplain_rhein
 #' 
 #' @title Active floodplain along River Rhine
 #' 
 #' @description This dataset contains a polygon of the active floodplain along
-#'    the German, freeflowing parts of River Rhine from the weir Iffezheim and 
-#'    the Dutch border in the coordinate reference system ETRS 1989 UTM 32N.
+#'   the German, freeflowing parts of River Rhine from the weir Iffezheim and 
+#'   the Dutch border in the coordinate reference system ETRS 1989 UTM 32N.
 #' 
-#'    Originally, this polygon was produced for the floodplain status report 
-#'    (Auenzustandsbericht; \insertCite{brunotte_flussauen_2009;textual}{hydflood3}; 
-#'    \insertCite{brunotte_flussauen_data_2009;textual}{hydflood3}; 
-#'    \insertCite{bfn_auenzustandsbericht_2009;textual}{hydflood3}) 
-#'    at a scale of 1:25000. For hydflood3 it was updated with recent flood 
-#'    protection measures and manually improved with recent digital 
-#'    elevation models and aerial images at a scale < 1:10000.
+#'   Originally, this polygon was produced for the floodplain status report 
+#'   (Auenzustandsbericht; Brunotte et al. (2009), BfN (2009)) 
+#'   at a scale of 1:25000. For hydflood3 it was updated with recent flood 
+#'   protection measures and manually improved with recent digital 
+#'   elevation models and aerial images at a scale < 1:10000.
 #' 
 #' @format A \code{SpatialPolygonsDataFrame} containing 1 polygon 
-#'
-#' @references \insertAllCited{}
-#'
+#' 
+#' @references 
+#'   \insertRef{brunotte_flussauen_2009}{hydflood3}
+#'   
+#'   \insertRef{brunotte_flussauen_data_2009}{hydflood3}
+#'   
+#'   \insertRef{bfn_auenzustandsbericht_2009}{hydflood3}
+#' 
 "spdf.active_floodplain_rhein"
