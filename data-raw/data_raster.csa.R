aa80345d3c2e74368dbfb9fca105ca709a69d816
Arnd Weber <arnd.weber@bafg.de>
2018-08-17 18:15:53 +0200

    load packages, if they haven't been loaded due to sourcing by _build.R

--- data-raw/data_raster.csa.R
+++ data-raw/data_raster.csa.R
@@ -1,10 +1,14 @@
 
-require("rgdal")
-require("sp")
-require("raster")
-
 # store raster.csa as external dataset
 if (!(file.exists("data/raster.csa.rda"))){
+    
+    if (!exists("lib")){
+        v <- R.Version()
+        lib <- paste0("~/R/", paste(sep = ".", v$major, v$minor))
+    }
+    require("rgdal", lib.loc = lib)
+    require("sp", lib.loc = lib)
+    require("raster", lib.loc = lib)
     
     # import
     raster.csa <- readAll(raster("data-raw/raster.csa.tif"))
