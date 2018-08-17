4fb46c9fd1f898bd7a6ed88ff2a8ad4a875c548f
Arnd Weber <arnd.weber@bafg.de>
2018-08-17 18:18:48 +0200

    added testthat to the installed packages

--- _install.R
+++ _install.R
@@ -21,8 +21,8 @@ dir.create(lib, verbose, TRUE)
 
 # install dependencies
 packages <- c("hyd1d", "rgdal", "sp", "raster", "rgeos", "Rdpack", "knitr",  
-              "rmarkdown", "devtools", "pkgdown", "roxygen2", "plot3D",
-              "plotrix")
+              "rmarkdown", "devtools", "pkgdown", "roxygen2", "testthat",
+              "plot3D", "plotrix")
 
 for (a_package in packages) {
     if (! (a_package %in% installed.packages(lib.loc = lib)[, "Package"])) {
