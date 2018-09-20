4eeadd8c82096733a80c8095642868341c730db1
Arnd Weber <arnd.weber@bafg.de>
2018-09-20 14:04:55 +0200

    added package shinycssloaders

--- _install.R
+++ _install.R
@@ -22,8 +22,8 @@ dir.create(lib, verbose, TRUE)
 # install dependencies
 packages <- c("hyd1d", "rgdal", "sp", "raster", "rgeos", "Rdpack", "knitr",  
               "rmarkdown", "devtools", "pkgdown", "roxygen2", "testthat",
-              "plot3D", "plotrix", "shiny", "shinyjs", "leaflet", 
-              "leaflet.extras", "leaflet.esri", "htmltools", "sendmailR")
+              "plot3D", "plotrix", "shiny", "shinyjs", "shinycssloaders", 
+              "leaflet", "leaflet.extras", "leaflet.esri", "htmltools")
 
 for (a_package in packages) {
     if (! (a_package %in% installed.packages(lib.loc = lib)[, "Package"])) {
