8af28a1cff879685a05dfd87ddde14e289869893
Arnd Weber <arnd.weber@bafg.de>
2019-01-23 13:09:17 +0100

    added package "usethis" taking over use_data function from "devtools"

--- data-raw/data_spdf.active_floodplain_rhein.R
+++ data-raw/data_spdf.active_floodplain_rhein.R
@@ -14,8 +14,8 @@ if (!(file.exists("data/spdf.active_floodplain_rhein.rda"))) {
                                             verbose = FALSE)
     
     # export
-    devtools::use_data(spdf.active_floodplain_rhein, pkg = ".", overwrite = TRUE, 
-                       compress = "bzip2")
+    usethis::use_data(spdf.active_floodplain_rhein, pkg = ".", overwrite = TRUE, 
+                      compress = "bzip2")
     
     # clean up
     rm(spdf.active_floodplain_rhein)
