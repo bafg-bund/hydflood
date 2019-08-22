c5d82bb54653a439ce729dd1ab2e14d5c39344aa
Arnd Weber <arnd.weber@bafg.de>
2019-08-22 09:58:30 +0200

    add shinyapp flood3daily

---
+++ shinyapps/flood3daily/ui.R
@@ -0,0 +1,49 @@
+library(shiny)
+
+fluidPage(
+    
+    column(width = 3,
+        
+        HTML("<BR>"),
+        HTML("<BR>"),
+        HTML("<BR>"),
+        
+        # menu item gauging_station
+        alignRight(selectInput(
+            inputId  = "gauging_station",
+            label    = "Bereich:",
+            choices  = gs,
+            selected = "DESSAU")),
+        
+        # menu item DATERANGE
+        alignRight(dateRangeInput("daterange", "Zeitraum:", 
+                       start = as.Date("2015-01-01"), end = date_max, 
+                       min = as.Date("2015-01-01"), max = date_max, 
+                       format = "dd-mm-yyyy",
+                       language = "de", separator = " bis ")),
+        
+        # menu item DATE
+        uiOutput("menu_date"),
+        
+        # plot 
+        plotOutput("plot")
+        
+    ),
+    
+    column(width = 9,
+           align = "center",
+           h2(textOutput("title")),
+        
+        tags$style(
+            type="text/css",
+            "#image img {
+                max-width: 90%;
+                width: 90%; 
+                height: auto;
+            }"
+        ),
+        
+        imageOutput("image")
+    )
+    
+)
