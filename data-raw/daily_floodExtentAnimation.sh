cb0e833127e52167ba99905ecdf87f2feffbda14
Arnd Weber <arnd.weber@bafg.de>
2019-09-19 06:52:09 +0200

    modify module name

--- data-raw/daily_floodExtentAnimation.sh
+++ data-raw/daily_floodExtentAnimation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /etc/profile.d/modules.sh
 module purge
-module load i4/applications/R-OS
+module load i4/R/OS
 module list
 cd /home/WeberA/hydflood3
 Rscript data-raw/daily_floodExtentAnimation.R > data-raw/daily_floodExtentAnimation.log 2>&1
