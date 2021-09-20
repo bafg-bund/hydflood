716864cffe6da338e064e6320e97c0426c35e967
Arnd Weber <arnd.weber@bafg.de>
2021-09-20 10:39:39 +0200

    compute flood durations back to 1960

--- slurm/flood3_elbe.R
+++ slurm/flood3_elbe.R
@@ -23,13 +23,14 @@ library(hydflood, lib.loc = paste0("~/R/", getRversion()))
 spdf.tile <- spdf.tiles_elbe[i, ]
 hfc <- hydflood_cache$cache_path_get()
 n <- spdf.tile$name
-if (!n %in% c("e036_SANDAU2", "e038_WITTENBERGE", "e039_MUEGGENDORF",
-             "e040_SCHNACKENBURG", "e041_LENZEN", "e042_GORLEBEN",
-             "e043_DOEMITZ", "e044_HITZACKER", "e045_NEU_DARCHAU",
-             "e046_BLECKEDE")) {
-  stdout("not included in the computations")
-  q("no")
-}
+# if (!n %in% c(#"e036_SANDAU2", "e038_WITTENBERGE", "e039_MUEGGENDORF",
+#              #"e040_SCHNACKENBURG", "e041_LENZEN", "e042_GORLEBEN",
+#              "e043_DOEMITZ" #, "e044_HITZACKER", "e045_NEU_DARCHAU",
+#              #"e046_BLECKEDE")) {
+#              )) {
+#   stdout("not included in the computations")
+#   q("no")
+# }
 dem <- paste0(hfc, "/", n, "_DEM.tif")
 csa <- paste0(hfc, "/", n, "_CSA.tif")
 
@@ -37,7 +38,7 @@ csa <- paste0(hfc, "/", n, "_CSA.tif")
 # loop over the available years to produce annual flood3 rasters
 products <- list()
 
-for (a_year in 1960:1989) { #(as.numeric(strftime(Sys.Date(), "%Y")) - 1)) {
+for (a_year in 1960:(as.numeric(strftime(Sys.Date(), "%Y")) - 1)) {
   print(a_year)
   
   # output file
@@ -65,50 +66,73 @@ for (a_year in 1960:1989) { #(as.numeric(strftime(Sys.Date(), "%Y")) - 1)) {
 #####
 # aggregate selected years by mean() and sd()
 # annual prolongation of temporal series from 1990 until last year
-# f_mean <- paste0(o, "/", n, "_flood3_mean_1990_",
-#                  as.numeric(strftime(Sys.Date(), "%Y")) - 1, ".tif")
-# 
-# if (file.exists(f_mean)) {
-#   print(paste0(f_mean, " exists already"))
-# } else {
-#   print(paste0(f_mean, " will be computed"))
-#   calc(stack(products), fun = mean, na.rm = TRUE, filename = f_mean,
-#        format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
-# }
-# 
-# f_sd <- paste0(o, "/", n, "_flood3_sd_1990_",
-#                as.numeric(strftime(Sys.Date(), "%Y")) - 1, ".tif")
-# 
-# if (file.exists(f_sd)) {
-#   print(paste0(f_sd, " exists already"))
-# } else {
-#   print(paste0(f_sd, " will be computed"))
-#   calc(stack(products), fun = sd, na.rm = TRUE, filename = f_sd,
-#        format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
-# }
-# 
-# # reference period 1990 - 2019
-# f_mean <- paste0(o, "/", n, "_flood3_mean_1990_2019.tif")
-# 
-# if (file.exists(f_mean)) {
-#   print(paste0(f_mean, " exists already"))
-# } else {
-#   print(paste0(f_mean, " will be computed"))
-#   calc(stack(products[paste0("flood3_", 1990:2019)]),
-#        fun = mean, na.rm = TRUE, filename = f_mean,
-#        format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
-# }
-# 
-# f_sd <- paste0(o, "/", n, "_flood3_sd_1990_2019.tif")
-# 
-# if (file.exists(f_sd)) {
-#   print(paste0(f_sd, " exists already"))
-# } else {
-#   print(paste0(f_sd, " will be computed"))
-#   calc(stack(products[paste0("flood3_", 1990:2019)]),
-#        fun = sd, na.rm = TRUE, filename = f_sd,
-#        format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
-# }
+f_mean <- paste0(o, "/", n, "_flood3_mean_1990_",
+                 as.numeric(strftime(Sys.Date(), "%Y")) - 1, ".tif")
+
+if (file.exists(f_mean)) {
+  print(paste0(f_mean, " exists already"))
+} else {
+  print(paste0(f_mean, " will be computed"))
+  calc(stack(products), fun = mean, na.rm = TRUE, filename = f_mean,
+       format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
+}
+
+f_sd <- paste0(o, "/", n, "_flood3_sd_1990_",
+               as.numeric(strftime(Sys.Date(), "%Y")) - 1, ".tif")
+
+if (file.exists(f_sd)) {
+  print(paste0(f_sd, " exists already"))
+} else {
+  print(paste0(f_sd, " will be computed"))
+  calc(stack(products), fun = sd, na.rm = TRUE, filename = f_sd,
+       format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
+}
+
+# reference period 1960 - 1989
+f_mean <- paste0(o, "/", n, "_flood3_mean_1960_1989.tif")
+
+if (file.exists(f_mean)) {
+  print(paste0(f_mean, " exists already"))
+} else {
+  print(paste0(f_mean, " will be computed"))
+  calc(stack(products[paste0("flood3_", 1960:1989)]),
+       fun = mean, na.rm = TRUE, filename = f_mean,
+       format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
+}
+
+f_sd <- paste0(o, "/", n, "_flood3_sd_1960_1989.tif")
+
+if (file.exists(f_sd)) {
+  print(paste0(f_sd, " exists already"))
+} else {
+  print(paste0(f_sd, " will be computed"))
+  calc(stack(products[paste0("flood3_", 1960:1989)]),
+       fun = sd, na.rm = TRUE, filename = f_sd,
+       format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
+}
+
+# reference period 1990 - 2019
+f_mean <- paste0(o, "/", n, "_flood3_mean_1990_2019.tif")
+
+if (file.exists(f_mean)) {
+  print(paste0(f_mean, " exists already"))
+} else {
+  print(paste0(f_mean, " will be computed"))
+  calc(stack(products[paste0("flood3_", 1990:2019)]),
+       fun = mean, na.rm = TRUE, filename = f_mean,
+       format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
+}
+
+f_sd <- paste0(o, "/", n, "_flood3_sd_1990_2019.tif")
+
+if (file.exists(f_sd)) {
+  print(paste0(f_sd, " exists already"))
+} else {
+  print(paste0(f_sd, " will be computed"))
+  calc(stack(products[paste0("flood3_", 1990:2019)]),
+       fun = sd, na.rm = TRUE, filename = f_sd,
+       format = "GTiff", options = c("COMPRESS=LZW", "TFW=NO"))
+}
 
 #####
 # quit
