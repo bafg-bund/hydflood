97bd4ba0f49a89f893b8a141d1c9f0d31fa856a7
Arnd Weber <arnd.weber@bafg.de>
2022-06-13 10:40:28 +0200

    add classifyToPNV()

---
+++ R/classifyToPNV.R
@@ -0,0 +1,157 @@
+#' @name classifyToPNV
+#' @rdname classifyToPNV
+#' 
+#' @title Function to reclassify flood durations to potential natural vegetation
+#' 
+#' @description This function is a wrapper to the function
+#'   \code{\link[terra]{classify}} to convert flood durations computed with
+#'   \code{flood3} into potential natural vegetation (PNV) distributions using
+#'   reclassification rules supplied with \code{df.pnv}. Alternative
+#'   reclassification rules may be applied, but they must match column names and
+#'   types as given by \code{df.pnv}. \code{\link[terra]{classify}} is called
+#'   with \code{include.lowest = TRUE}, \code{right = FALSE} and 
+#'   \code{othersNA = TRUE}.
+#' 
+#' @param x argument of type \code{\link[terra]{SpatRaster}}.
+#' @param rcl optional argument of type \code{data.frame} with columns and
+#'    column types as specified in \code{df.pnv}.
+#' @param filename supplies an optional output filename and has to be type
+#'   \code{character}.
+#' @param \dots additional arguments as for \code{\link[terra]{writeRaster}}.
+#' 
+#' @return SpatRaster object containing potential natural vegetation 
+#'   distibution.
+#' 
+#' @references 
+#'   \insertRef{ochs_potential_2020}{hydflood}
+#' 
+#' @examples \dontrun{
+#' library(hydflood)
+#' 
+#' # import the raster data and create a raster stack
+#' c <- crs("EPSG:25833")
+#' e <- ext(309000, 310000, 5749000, 5750000)
+#' x <- hydSpatRaster(ext = e, crs = c)
+#' 
+#' # create a temporal sequence
+#' seq <- seq(as.Date("2016-12-01"), as.Date("2016-12-31"), by = "day")
+#' 
+#' # compute a flood duration
+#' fd <- flood3(x = x, seq = seq)
+#' 
+#' # reclassify to PNV
+#' pnv <- classifyToPNV(fd)
+#' }
+#' 
+#' @export
+#' 
+classifyToPNV <- function(x, rcl = NULL, filename = "", ...) {
+    
+    #####
+    # validate the input data
+    ##
+    # vector and function to catch error messages
+    errors <- character()
+    l <- function(errors) {as.character(length(errors) + 1)}
+    
+    ##
+    # x
+    if (class(x) != "SpatRaster") {
+        errors <- c(errors, paste0("Error ", l(errors), ": 'x' must be type 'S",
+                                   "patRaster'."))
+    }
+    if (length(x) != 1) {
+        errors <- c(errors, paste0("Error ", l(errors), ": 'length(x)' must be",
+                                   " 1."))
+    }
+    if (minmax(x)[1,] != 0) {
+        errors <- c(errors, paste0("Error ", l(errors), ": 'minmax(x)[1,]' mu",
+                                   "st be 0."))
+    }
+    if (minmax(x)[2,] >= 365 & minmax(x)[2,] <= 366) {
+        warning("'minmax(x)[2,]' should be between 365 or 366 (a full year).")
+    }
+    if (l(errors) != "1") {stop(paste0(errors, collapse="\n  "))}
+    
+    ##
+    # rcl
+    if (!is.null(rcl)) {
+        if (class(rcl) != "data.frame") {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'rcl' must be t",
+                                       "ype 'data.frame'."))
+        }
+        if (!"from" %in% names(rcl)) {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'names(rcl)' mu",
+                                       "st contain 'from'."))
+        }
+        if (!"to" %in% names(rcl)) {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'names(rcl)' mu",
+                                       "st contain 'to'."))
+        }
+        if (!"class" %in% names(rcl)) {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'names(rcl)' mu",
+                                       "st contain 'class'."))
+        }
+        if (!"vegtype" %in% names(rcl)) {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'names(rcl)' mu",
+                                       "st contain 'vegtype'."))
+        }
+        if (l(errors) != "1") {stop(paste0(errors, collapse="\n  "))}
+        
+        if (class(rcl$from) != "numeric" & class(rcl$from) != "integer") {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'class(rcl$from",
+                                       ")' must be either 'numeric' or 'intege",
+                                       "r'."))
+        }
+        if (class(rcl$to) != "numeric" & class(rcl$to) != "integer") {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'class(rcl$to",
+                                       ")' must be either 'numeric' or 'intege",
+                                       "r'."))
+        }
+        if (class(rcl$class) != "integer") {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'class(rcl$from",
+                                       ")' must be 'integer'."))
+        }
+        if (class(rcl$vegtype) != "character") {
+            errors <- c(errors, paste0("Error ", l(errors), ": 'class(rcl$from",
+                                       ")' must be 'character'."))
+        }
+        if (l(errors) != "1") {stop(paste0(errors, collapse="\n  "))}
+        
+        if (!all(sapply(list(order(rcl$to), order(rcl$class)),
+                        FUN = identical, order(rcl$from)))) {
+            errors <- c(errors, paste0("Error ", l(errors), ": The columns fro",
+                                       "m, to and class must have the same ord",
+                                       "er."))
+        }
+    } else {
+        rcl <- df.pnv
+    }
+    
+    ##
+    # filename
+    if (class(filename) != "character") {
+        errors <- c(errors, paste0("Error ", l(errors), ": 'filename' ",
+                                   "must be type 'character'."))
+    }
+    if (length(filename) != 1) {
+        errors <- c(errors, paste0("Error ", l(errors), ": 'filename' ",
+                                   "must have length 1."))
+    }
+    
+    if (l(errors) != "1") {stop(paste0(errors, collapse="\n  "))}
+    
+    # reclassify
+    rcl <- rcl[order(rcl$class), ]
+    r <- rcl[, c("from", "to", "class")]
+    
+    res <- classify(x, r, include.lowest = TRUE, right = FALSE, 
+                    othersNA = TRUE)
+    res <- res - 1
+    levels(res) <- rcl$vegtype
+    names(res) <- "PNV"
+    
+    writeRaster(res, filename = filename, ...)
+    
+    return(res)
+}
