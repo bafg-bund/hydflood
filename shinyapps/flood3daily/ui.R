a04ff6e249cb2eb6d9dd5218fb2e2940e301e011
Arnd Weber <arnd.weber@bafg.de>
2022-07-05 12:18:05 +0200

    make shinyapps bilingual

--- shinyapps/flood3daily/ui.R
+++ shinyapps/flood3daily/ui.R
@@ -1,5 +1,3 @@
-library(shiny)
-
 fluidPage(
     
     column(width = 3,
@@ -8,21 +6,9 @@ fluidPage(
         HTML("<BR>"),
         HTML("<BR>"),
         
-        # menu item gauging_station
-        alignRight(selectInput(
-            inputId  = "area",
-            label    = "Bereich:",
-            choices  = gs,
-            selected = "DESSAU")),
-        
-        # menu item DATERANGE
-        alignRight(dateRangeInput("daterange", "Zeitraum:", 
-                       start = as.Date("2015-01-01"), end = date_max, 
-                       min = as.Date("2015-01-01"), max = date_max, 
-                       format = "dd-mm-yyyy",
-                       language = "de", separator = " bis ")),
-        
-        # menu item DATE
+        # menu items
+        uiOutput("menu_area"),
+        uiOutput("menu_daterange"),
         uiOutput("menu_date"),
         
         # plot 
@@ -43,7 +29,7 @@ fluidPage(
             }"
         ),
         
-        imageOutput("image")
+        htmlOutput("image")
     )
     
 )
