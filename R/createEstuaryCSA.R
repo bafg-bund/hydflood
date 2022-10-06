38c3724728f080d10f1a8e356da6dca390e2bb7e
Arnd Weber <arnd.weber@bafg.de>
2022-10-06 16:50:52 +0200

    create a first empty template for createEstuaryCSA()

---
+++ R/createEstuaryCSA.R
@@ -0,0 +1,24 @@
+#' @name createEstuaryCSA
+#' @rdname createEstuaryCSA
+#' 
+#' @title Function to create estuary cross section areas
+#' 
+#' @description To ...
+#' 
+#' @param x has to by type \code{sf}.
+#' 
+#' @return \code{sf} containing ...
+#' 
+#' @examples
+#' \dontrun{
+#'   library(hydflood)
+#'   axis <- st_read("data-raw/axis.shp")
+#'   createEstuaryCSA(axis)
+#' }
+#' 
+#' @export
+#' 
+createEstuaryCSA <- function(x) {
+    
+    return(x)
+}
