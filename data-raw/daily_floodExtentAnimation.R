de13b628b6bbb955aaadc92bbe18efadf9eed6cc
Arnd Weber <arnd.weber@bafg.de>
2023-05-02 09:34:06 +0200

    added Löderitz to the flood3daily-ShinyApp

--- data-raw/daily_floodExtentAnimation.R
+++ data-raw/daily_floodExtentAnimation.R
@@ -291,7 +291,88 @@ if (require("ElBiota")) {
     }
 }
 
+#####
+# LÖDERITZ
+ext_L <- ext(291047, 295500, 5750500, 5755500)
+#dem <- getDEM("data-raw/raster.dem_loederitz.tif", ext_L, st_crs("EPSG:25833"))
+dem <- rast("data-raw/raster.dem_loederitz.tif")
+dem_plot <- dem
+dem_plot[dem_plot > 60] <- NA
+dem_plot[dem_plot < 42] <- 42
+csa <- rast("data-raw/raster.csa_loederitz.tif")
+x <- hydSpatRaster(filename_dem = "data-raw/raster.dem_loederitz.tif",
+                   filename_csa = "data-raw/raster.csa_loederitz.tif",
+                   ext = ext_L, crs = st_crs("EPSG:25833"))
 
+# mask <- st_read(dsn = "data-raw", layer = "doc_mask_lenzen")
+# mask <- st_transform(mask, wgs84)
+# mask$SHAPE_Leng <- NULL
+# mask$SHAPE_Area <- NULL
+# mask_floodplain <- st_read(dsn = "data-raw", layer="doc_mask_lenzen_floodplain")
+# mask_floodplain <- st_transform(mask_floodplain, wgs84)
+# mask_floodplain$SHAPE_Leng <- NULL
+# mask_floodplain$SHAPE_Area <- NULL
+# mask_floodplain <- st_intersection(mask_floodplain, mask)
+# extent_wgs84 <- ext(mask)
+
+# export a dem plot
+dem <- paste0(p, "/LOEDERITZ/dem.png")
+if (!file.exists(dem)) {
+    png(filename = dem, width = 960, height = 640, units = "px")
+    plot(raster(dem_plot), col = dem_colfunc((60 - 42)*2), xlim = c(291000, 296000),
+         legend.width = 1, horizontal = TRUE, bty = "n",
+         legend.args = list(text = "elevation (m)"),
+         xaxp = c(291000, 296000, 5), yaxp = c(5751000, 5755000, 4))
+    # plot(dem_plot, col = dem_colfunc((24 - 9)*2), xlim = c(263500, 268800),
+    #      bty = "n", xaxp = c(264000, 268000, 2), yaxp = c(5884000, 5886000, 1),
+    #      plg = list(title = "elevation (m)", horiz = TRUE, width = 1))
+    # plot(sf.gsd_L$geometry, pch = 21, bg = "white", add =TRUE)
+    # text(st_coordinates(sf.gsd_L[1, ]), pos = 3,
+    #      labels = sf.gsd_L$gauging_station[1])
+    # text(st_coordinates(sf.gsd_L[2, ]), pos = 1,
+    #      labels = sf.gsd_L$gauging_station[2])
+    dev.off()
+}
+
+# loop over all dates
+for (a_date in dates) {
+    
+    write(paste0("LOEDERITZ: ", a_date), stdout())
+    
+    f_out <- paste0(p, "/LOEDERITZ/flood3_", gsub("-", "", a_date), ".png")
+    
+    if (file.exists(f_out)) {
+        write("  exists already", stdout())
+    } else {
+        write("  will be computed", stdout())
+        
+        # compute flood extent
+        # flood_extent <- tryCatch({
+        flood_extent <- flood3(x, as.Date(a_date))
+        flood_extent[flood_extent == 0] <- NA
+        # },
+        # error = function(cond) {NA})
+        
+        # plotting with raster functions
+        if (inherits(flood_extent, "SpatRaster")) {
+            png(filename = f_out, width = 960, height = 640, units = "px")
+            plot(raster(dem_plot), col = dem_colfunc((60 - 42)*2), xlim = c(291000, 296000),
+                 legend.width = 1, horizontal = TRUE, bty = "n",
+                 legend.args = list(text = "elevation (m)"),
+                 xaxp = c(291000, 296000, 5), yaxp = c(5751000, 5755000, 4))
+            # plot(dem_plot, col = dem_colfunc((24 - 9)*2), xlim = c(263500, 268800),
+            #      bty = "n", xaxp = c(264000, 268000, 2), yaxp = c(5884000, 5886000, 1),
+            #      plg = list(title = "elevation (m)", horiz = TRUE, width = 1))
+            plot(raster(flood_extent), col = "blue", add = TRUE, legend = FALSE)
+            # plot(sf.gsd_L$geometry, pch = 21, bg = "white", add =TRUE)
+            # text(st_coordinates(sf.gsd_L[1, ]), pos = 3,
+            #      labels = sf.gsd_L$gauging_station[1])
+            # text(st_coordinates(sf.gsd_L[2, ]), pos = 1,
+            #      labels = sf.gsd_L$gauging_station[2])
+            dev.off()
+        }
+    }
+}
 
 #
 q("no")
